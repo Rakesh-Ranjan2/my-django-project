d53f74e89b4722e0ce1f12b83d52fca9c952d11e
Atharva <itsatharva.gupta@gmail.com>
2025-10-22 10:23:37 +0530

    Initial commit - clean Django ML project

---
+++ mlapp/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from tensorflow.keras.models import load_model # type: ignore
+import numpy as np
+from PIL import Image
+
+
+# Load model once when server starts
+model = load_model('mlapp/prototype1.h5')
+
+class_names  = [
+    "Apple___Apple_scab",
+    "Apple___Black_rot",
+    "Apple___Cedar_apple_rust",
+    "Apple___healthy",
+    "Blueberry___healthy",
+    "Cherry_(including_sour)___Powdery_mildew",
+    "Cherry_(including_sour)___healthy",
+    "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot",
+    "Corn_(maize)___Common_rust_",
+    "Corn_(maize)___Northern_Leaf_Blight",
+    "Corn_(maize)___healthy",
+    "Grape___Black_rot",
+    "Grape___Esca_(Black_Measles)",
+    "Grape___Leaf_blight_(Isariopsis_Leaf_Spot)",
+    "Grape___healthy",
+    "Orange___Haunglongbing_(Citrus_greening)",
+    "Peach___Bacterial_spot",
+    "Peach___healthy",
+    "Pepper,_bell___Bacterial_spot",
+    "Pepper,_bell___healthy",
+    "Potato___Early_blight",
+    "Potato___Late_blight",
+    "Potato___healthy",
+    "Raspberry___healthy",
+    "Soybean___healthy",
+    "Squash___Powdery_mildew",
+    "Strawberry___Leaf_scorch",
+    "Strawberry___healthy",
+    "Tomato___Bacterial_spot",
+    "Tomato___Early_blight",
+    "Tomato___Late_blight",
+    "Tomato___Leaf_Mold",
+    "Tomato___Septoria_leaf_spot",
+    "Tomato___Spider_mites Two-spotted_spider_mite",
+    "Tomato___Target_Spot",
+    "Tomato___Tomato_Yellow_Leaf_Curl_Virus",
+    "Tomato___Tomato_mosaic_virus",
+    "Tomato___healthy"
+]
+
+def predict_view(request):
+    prediction = None
+    certainty = None
+
+    if request.method == 'POST' and request.FILES.get('img'):
+        img_file = request.FILES['img']
+        img = Image.open(img_file).resize((224, 224))
+        img_array = np.array(img) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
+
+        result = model.predict(img_array)
+        pred_index = np.argmax(result)
+        prediction = class_names[pred_index]
+        certainty =  round(float(result[0][pred_index]) * 100, 2) 
+
+    return render(request, 'mlapp/predict.html', {'result': prediction, 'accuracy': certainty})
+
