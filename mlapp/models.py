e663ff0a5fe532f3f58c8175d2499d5332a9aeb6
Atharva <itsatharva.gupta@gmail.com>
2025-10-22 17:42:38 +0530

    stuff

--- mlapp/models.py
+++ mlapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class PlantImage(models.Model):
     image = models.ImageField(upload_to='uploads/')
     prediction = models.CharField(max_length=100, blank=True, null=True)
-    certainity = models.FloatField(blank=True, null=True)
+    certainity = models.CharField(blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- myproject/settings.py
+++ myproject/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mlapp',
-    'django_bootstrap5',
 
     'django.contrib.admin',
     'django.contrib.auth',
