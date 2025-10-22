b9b837e03dd7f59b4535998c93a961f2be0d0b52
Atharva <itsatharva.gupta@gmail.com>
2025-10-22 19:06:54 +0530

    FINAL

--- mlapp/admin.py
+++ mlapp/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import PlantImage
 
-@admin.register(PlantImage)
-class PlantImageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'image', 'prediction', 'certainity', 'uploaded_at')
+
